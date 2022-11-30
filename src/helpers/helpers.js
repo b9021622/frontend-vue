9971679ac5fda7b9f6c4386e674222993b57d338
b9021622 <74154256+b9021622@users.noreply.github.com>
2022-11-30 22:29:43 +0000

    3050EDIT

--- src/helpers/helpers.js
+++ src/helpers/helpers.js
@@ -11,7 +11,7 @@ Vue.use(VueFlashMessage, {
 });
 
 const vm = new Vue();
-const baseURL = 'http://localhost:8081/login';
+const baseURL = 'http://localhost:3050/';
 
 const handleError = fn => (...params) =>
     fn(...params).catch(error => {
